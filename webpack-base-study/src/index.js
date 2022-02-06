b331c3ebeb13eb952336d1e3c8a6e49638dac796
BubbleTg <tg_z@qq.com>
2022-02-07 00:35:45 +0800

    feat: 基本配置

--- webpack-base-study/src/index.js
+++ webpack-base-study/src/index.js
@@ -1,3 +1,26 @@
+import './index.css';
+import './01.less';
+import './02.scss';
+let title = require('./title');
+const content = require('./content.txt');
+console.log('🚀 ~ file: index.js ~ line 4 ~ content', content);
+console.log('helow', title);
 
-let title = require('./title')
-console.log('helow',title);
+console.log('🚀 ~ file: index.js ~ line 7 ~ process.env.NODE_ENV', process.env.NODE_ENV);
+console.log('🚀 ~ file: index.js ~ line 7 ~ NODE_ENV', NODE_ENV);
+
+// let imageUrl = require('./image/Snipaste_2022-02-06_23-05-41.png');
+// let img = new Image();
+// img.src = imageUrl.default;
+// document.body.appendChild(img);
+
+function readonly(target, key, descriptors) {
+  descriptors.writable = false;
+}
+class Person {
+  @readonly Pi = 3.14;
+}
+
+let p = new Person();
+p.Pi = 3.15;
+console.log("🚀 ~ file: index.js ~ line 25 ~ p", p)
--- webpack-base-study/webpack.config.js
+++ webpack-base-study/webpack.config.js
@@ -1,7 +1,98 @@
-
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js'
-}
+module.exports = (env) => {
+  return {
+    mode: process.env.NODE_ENV,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      publicPath: '',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                ['@babel/plugin-proposal-decorators', { legacy: true }], // legacy 旧的意思
+                ['@babel/plugin-proposal-class-properties', { loose: true }], // loose 松散的意思
+              ],
+            },
+          },
+        },
+        {
+          test: /\.txt$/,
+          type: 'asset/source',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                // 启用/禁用 url() 处理
+                url: true,
+                // 启用/禁用 @import 处理
+                import: true,
+                // 启用/禁用 Sourcemap
+                sourceMap: false,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.less$/,
+          use: ['style-loader', 'css-loader', 'less-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          // use: [
+          //   {
+          //     loader: 'url-loader',
+          //     options: {
+          //       name: '[hash:10].[ext]',
+          //       esModule: false, // 是否包装成一个 ES6 模块
+          //       limit: 8*1024,
+          //     },
+          //   },
+          // ],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    devServer: {
+      // 配置额外的静态根目录
+      // contentBase: path.resolve(__dirname, 'public'),
+      compress: true, // 启动 gzip 压缩
+      port: 8080,
+      // open: true,
+    },
+    plugins: [
+      // 生成一个 index.html 文件，插入脚本
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
+  };
+};
